c054b483dc999f79ed3dcdfa7ec49ac761c5d962
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2020-07-03 15:13:39 -0300

    Removendo o operador this-> da classe abstrata

--- source/AbstractOdeSolver.cpp
+++ source/AbstractOdeSolver.cpp
@@ -3,19 +3,19 @@
 
 //Defines the size of the step between each iteraction in the caculation.
 void AbstractOdeSolver::SetStepSize(double h) {
-    this->stepSize = h;
+    stepSize = h;
 }
 
 //Defines the domain of the function i.e. the initial time and the final one.
 void AbstractOdeSolver::SetTimeInterval(double t0, double t1) {
     assert(t1 > t0);
-    this->initialTime = t0;
-    this->finalTime = t1;
+    initialTime = t0;
+    finalTime = t1;
 }
 
 //Defines the inicial condition of the model when t=0.
 void AbstractOdeSolver::SetInitialValue(double y0) {
-    this->initialValue = y0;
+    initialValue = y0;
 }
 
 double AbstractOdeSolver::GetStepSize() {
